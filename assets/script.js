d22f6130c312e7067f52c71ef6e62db88994a589
Alicia Gorton <apersigehl@gmail.com>
2020-08-02 15:52:42 -0700

    changed styling on jumbotron

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,29 @@
+// A $(docment).ready()block.
 $(document).ready(function(){
     console.log("ready!");
+
     let current = moment().hour();
     let todos = []
+
+    // color change depending on time  
     $(".time-block").each(function(index){
-        let id = $(this).attr("id");
+        let id = ($(this).attr("id"));
+        
+        let hour = parseInt($(this).attr("data-hour"));
         console.log(`id: ${id}, current: ${current}`);
+
+        if(current > hour) {
+            $(this).addClass(".past")
+            console.log("Have a good morning!")
+        }
+        else if (current === hour) {
+            $(this).addClass(".present")
+            console.log("Current time")
+        }
+        else {
+            $(this).addClass(".future")
+            console.log("Have a night")
+        }
 
         // key id --> retrieve from local storage 
         var dataStored = localStorage.getItem(id);
@@ -16,15 +35,7 @@ $(document).ready(function(){
         $(this).find("textarea").val(dataStored);
 
 
-        if(current > id) {
-            $(this).addClass("past")
-        }
-        else if (current === id) {
-            $(this).addClass("present")
-        }
-        else {
-            $(this).addClass("future")
-        }
+       
     })
 
 })
@@ -43,20 +54,20 @@ $(".saveBtn").on("click", function(){
 })
 
 var currentTime = moment().hour()
-    for (let i = 8; i <= 16; i++) {
-        const element = localStorage.getItem(i);
-        $(`#${i}-input`).val(element)
-        if (currentTime > i ){
-            $(`#${i}`).addClass("bg-info")
-            console.log("Have a good morning!")
-        } else if (currentTime == i ){
-            $(`#${i}`).addClass("bg-warning")
-            console.log("Hey")
-        } else {
-            $(`#${i}`).addClass("bg-success")
-            console.log("Have a night")
-        }
+    // for (let i = 8; i <= 16; i++) {
+    //     const element = localStorage.getItem(i);
+    //     $(`#${i}-input`).val(element)
+    //     if (currentTime > i ){
+    //         $(`#${i}`).addClass("bg-info")
+    //         console.log("Have a good morning!")
+    //     } else if (currentTime == i ){
+    //         $(`#${i}`).addClass("bg-warning")
+    //         console.log("Hey")
+    //     } else {
+    //         $(`#${i}`).addClass("bg-success")
+    //         console.log("Have a night")
+    //     }
 
-    }
+    // }
 var todaysDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#todaysDate").append(todaysDate)
